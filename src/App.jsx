7e280b6a6a6e6b631e1da18ffe55e7fef8727954
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2022-07-26 11:22:44 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>App criptos</div>
+    </div>
+  );
+}
+
+export default App;
