d3a4a9b1932085e4be1fd640f2e8b2d86661c116
Rodriman28 <35462876+Rodriman28@users.noreply.github.com>
2022-07-26 18:33:54 -0300

    Finished app

---
+++ src/data/monedas.js
@@ -0,0 +1,9 @@
+const monedas = [
+  { id: "USD", nombre: "Dolar de los Estados Unidos" },
+  { id: "MXN", nombre: "Peso Mexicano" },
+  { id: "EUR", nombre: "Euro" },
+  { id: "GBP", nombre: "Libra Esterlina" },
+  { id: "UYU", nombre: "Peso Uruguayo" },
+];
+
+export { monedas };
